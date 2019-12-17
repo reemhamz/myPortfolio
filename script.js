06eec61a090bd2babb9448f74ead8d0f41791359
Reem H <reemhamoui92@gmail.com>
2019-12-17 12:39:55 -0500

    added animate on scroll

--- script.js
+++ script.js
@@ -2,8 +2,21 @@ AOS.init();
 
 // You can also pass an optional settings object
 // below listed default settings
-AOS.init({
+// AOS.init({
+//     // duration: 1200, // values from 0 to 3000, with step 50ms
+// });
 
-    duration: 1200, // values from 0 to 3000, with step 50ms
 
+$(document).ready(function () {
+    $(".menuIcon").on('click', function () {
+        $("nav").toggleClass("menu_show");
+    });
+
+    AOS.init(
+        {
+            duration: 800
+        }
+    );
 });
+
+
